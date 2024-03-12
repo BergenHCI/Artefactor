7a50d85339d78bbe605de69ec20b751cd7805ca6
Paul Okopny <paul.okopny@gmail.com>
2024-03-12 17:02:24 +0100

    add user story generation

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def new_data():
     st.session_state.data = data
 
 
+def extract_numerated_list(text:str) -> list:
+    # Regular expression to match numerated list items: 
+    # It looks for lines starting with one or more digits followed by a period.
+    pattern = re.compile(r'\b\d+\.\s*(.*)')
+    # Find all matches in the text
+    matches = pattern.findall(text)
+    if not matches:
+        st.error("Can not parse the use case scenario")
+    return matches
+
+
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
@@ -26,11 +37,19 @@ with st.sidebar:
         new_data()
         st.rerun()
 
+@st.cache_resource
+def get_client():
+    if not openai_api_key:
+        st.error("Please provide Open AI API key first")
+        st.stop()
+    client = OpenAI(api_key=openai_api_key)
+    return client
 
-@st.cache_data(show_spinner=True)
+
+@st.cache_data(show_spinner="Generating..")
 def generate_scenario(seed) -> str:
     data = st.session_state.data
-    client = OpenAI(api_key=openai_api_key)
+    client = get_client()
     message_parts = ["Create a realistic use case for the use of an application based on the following data:"]
     user_desc = data["app"].get("user_desc")
     app_desc = data["app"].get("app_desc")
@@ -49,7 +68,7 @@ def generate_scenario(seed) -> str:
         message_parts.append("The application solves the problem: %s" % problem)
         message_parts.append("The application description: %s" % app_desc)
         message_parts.append("The contect of use: %s" % context)
-        message_parts.append("Use numbered list to describe the steps in scenario. Focus on interaction between the user and the application.")
+        message_parts.append("Create a use case scenario of 5 or less steps. Use numbered list to describe the steps in scenario. Describe each step in one sentence. Focus on interaction between the user and the application.")
         messages=[
             {"role": "system", "content": "You are a UX design assistant, helping to create use cases and storyboards."},
             {"role": "user", "content": "\n".join(message_parts)}
@@ -61,27 +80,48 @@ def generate_scenario(seed) -> str:
 
 
 def generate_image(prompt:str):
-    client = OpenAI(api_key=openai_api_key)
+    client = get_client()
     response = client.images.generate(
-        model="dall-e-2",
+        model="dall-e-3",
         prompt=prompt,
-        size="256x256",
+        size="1024x1024",
         quality="standard",
         n=1,
     )
     return response.data[0].url
 
-def extract_numerated_list(text):
-    # Regular expression to match numerated list items: 
-    # It looks for lines starting with one or more digits followed by a period.
-    pattern = re.compile(r'\b\d+\.\s*(.*)')
-    # Find all matches in the text
-    matches = pattern.findall(text)
-    if not matches:
-        st.error("Can not parse the use case scenario")
-    return matches
 
-@st.cache_data(show_spinner=True)
+@st.cache_data(show_spinner="Generating..")
+def generate_user_stories(seed) -> list:
+    data = st.session_state.data
+    scenario = data.get("scenario")
+    if not scenario:
+        st.error("Generate scenario first")
+        return []
+    client = get_client()
+    if scenario:
+        message_parts = ["Create user stories following the template “As a [persona], I [want to], [so that].” based on the following:"]
+        user_desc = data["app"].get("user_desc")
+        app_desc = data["app"].get("app_desc")
+        problem = data["app"].get("problem")
+        context = data["app"].get("use_context")
+        if user_desc: message_parts.append("The user is described as following: %s" % user_desc)
+        if problem: message_parts.append("The application solves the problem: %s" % problem)
+        if app_desc: message_parts.append("The application description: %s" % app_desc)
+        if context: message_parts.append("The contect of use: %s" % context)
+        message_parts.append("The suggests scenario of use: %s" % scenario)
+        message_parts.append("Use numbered list for user stories.")
+        messages=[
+            {"role": "system", "content": "You are a UX design assistant, helping to create user stories"},
+            {"role": "user", "content": "\n".join(message_parts)}
+        ]
+        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+        msg = response.choices[0].message.content
+        return msg
+    return ""
+
+
+@st.cache_data(show_spinner="Generating..")
 def generate_storyboard(seed):
     data = st.session_state.data
     # find scenario
@@ -97,6 +137,7 @@ def generate_storyboard(seed):
         storyboard.append({"desc": step, "url": img_url})
     return storyboard
 
+
 if "data" not in st.session_state:
     new_data()
 
@@ -111,7 +152,7 @@ def scenario_editor():
 
 
 def scenario_preview():
-    st.header("Scenario")
+    # st.header("Scenario")
     data = st.session_state.data
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
@@ -123,18 +164,29 @@ def scenario_preview():
         st.write(data["scenario"])
 
 def storyboard_preview():
-    st.header("Storyboard")
+    # st.header("Storyboard")
     data = st.session_state.data
     if st.button("Generate storyboard"):
         seed = hashlib.sha256(str(sorted(data.items())).encode()).hexdigest()
         data["storyboard"] = generate_storyboard(seed)
     if "storyboard" in data:
         for step in data["storyboard"]:
-            st.image(step["url"], caption=step["desc"], width=256)
+            st.image(step["url"], caption=step["desc"], width=512)
 
+def userstory_preview():
+    data = st.session_state.data
+    if st.button("Generate user stories"):
+        seed = hashlib.sha256(str(sorted(data.items())).encode()).hexdigest()
+        data["stories"] = generate_user_stories(seed)
+    if "stories" in data:
+        st.write(data["stories"])
 
 with st.container():
     scenario_editor()
-    scenario_preview()
-    storyboard_preview()
     
+    with st.expander("Scenario"):
+        scenario_preview()
+    with st.expander("Storyboard"):
+        storyboard_preview()
+    with st.expander("User stories"):
+        userstory_preview()
